dde94edd7bac31532a3f20dee5339b76fa53d420
techyyash <57702209+techyyash@users.noreply.github.com>
2024-09-09 14:02:17 +0530

    Update settings.py

--- resume_app/resume_app/settings.py
+++ resume_app/resume_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
